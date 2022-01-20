ff689a12643f39d2bbc25de768d0e90dd242f67d
Jacob Blaettler <jrblaettler@gmail.com>
2022-01-20 16:19:38 -0700

    Changes committed:
    	modified:   server/src/routes/index.js

--- server/src/routes/index.js
+++ server/src/routes/index.js
@@ -6,4 +6,8 @@ router.get('/api', (req, res) => {
   res.status(200).send();
 });
 
+router.get('/loaderio-c3bdd87010253726a299357313cd1f87', (req, res) => {
+  res.send('loaderio-c3bdd87010253726a299357313cd1f87');
+});
+
 module.exports = router;
